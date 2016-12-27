2fbf0d0afa3b3686e29e11dbdc20cea875482e62
Tiago Marques <tiago.map.marques@gmail.com>
2016-12-27 18:35:51 +0000

    updated home page

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BooksService } from '../shared/services/index';
+import { BookListState, GenreState, CategoryState } from '../shared/states/index';
 import { Book } from '../shared/models/index';
 
 /**
@@ -13,49 +13,103 @@ import { Book } from '../shared/models/index';
 })
 
 export class HomeComponent implements OnInit {
+  private books: Book[];
+  private genres: string[];
+  private categories: string[];
 
-  newName: string = '';
-  errorMessage: string;
-  names: any[] = [];
+  private titleFilter: string;
+  private selectedGenres: number[];
+  private selectedCategory: number;
+
+  private booksPerPage: number;
+  private pageNumber: number;
 
   /**
    * Creates an instance of the HomeComponent with the injected
-   * NameListService.
+   * BooksService.
    *
-   * @param {NameListService} nameListService - The injected NameListService.
+   * @param {BooksService} booksService - The injected BooksService.
    */
-  constructor(public booksService: BooksService) {}
+  constructor(
+    private bookListState: BookListState,
+    private genreState: GenreState,
+    private categoryState: CategoryState
+  ) {}
 
   /**
    * Get the names OnInit
    */
-  ngOnInit() {
-    this.getNames();
+  public ngOnInit(): void {
+    this.bookListState.get().subscribe(books => this.books = books);
+    this.genreState.get().subscribe(genres => this.genres = genres);
+    this.categoryState.get().subscribe(categories => this.categories = categories);
+    this.titleFilter = '';
+    this.selectedGenres = [];
+    this.selectedCategory = null;
+    this.booksPerPage = 16;
+    this.pageNumber = 1;
   }
 
-  /**
-   * Handle the nameListService observable
-   */
-  getNames() {
-    this.booksService.get().subscribe((books: Book[]) => {
-      console.log(books);
+  public titleChanged(): void {
+    this.updateBookList();
+  }
+
+  public genreChanged(genres: number[]): void {
+    this.selectedGenres = genres;
+    this.updateBookList();
+  }
+
+  public categoryChanged(category: number): void {
+    this.selectedCategory = category;
+    this.updateBookList();
+  }
+
+  public isWithinPagination(index: number) {
+    return (index >= (this.pageNumber-1) * this.booksPerPage) && (index < this.pageNumber * this.booksPerPage);
+  }
+
+  public isPreviousPageValid(): boolean {
+    return this.pageNumber > 1;
+  }
+
+  public isNextPageValid() {
+    return this.books && (this.books.length / this.booksPerPage) > this.pageNumber;
+  }
+
+  public previousPage(): void {
+    this.pageNumber--;
+  }
+
+  public nextPage(): void {
+    this.pageNumber++;
+  }
+
+  private updateBookList(): void {
+    this.bookListState.get().subscribe((books: Book[]) => {
+      this.books = books.filter(book => {
+        let matchesTitleOrAuthor = this.fineSearch(book.name, this.titleFilter);
+        let matchesGenre = false;
+        if(this.selectedGenres.length === 0) {
+          matchesGenre = true;
+        } else {
+          this.selectedGenres.forEach((genreIndex: number) => {
+            matchesGenre = matchesGenre || this.fineSearch(book.genre.name, this.genres[genreIndex]);
+          });
+        }
+        let matchesCategory =
+          (this.selectedCategory === null) ||
+          this.fineSearch(book.genre.category, this.categories[this.selectedCategory], true);
+        return matchesTitleOrAuthor && matchesGenre && matchesCategory;
+      });
     });
-    // this.nameListService.get()
-    //   .subscribe(
-    //     names => this.names = names,
-    //     error =>  this.errorMessage = <any>error
-    //   );
   }
 
-  /**
-   * Pushes a new name onto the names array
-   * @return {boolean} false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    // TODO: implement nameListService.post
-    this.names.push(this.newName);
-    this.newName = '';
-    return false;
+  private fineSearch(value: string, query: string, matchStart: boolean = false): boolean {
+    let index = this.trimAndLower(value).indexOf(this.trimAndLower(query));
+    return matchStart ? index === 0 : index !== -1;
   }
 
+  private trimAndLower(value: string): string {
+    return value.replace(/[^a-zA-Z][^a-zA-Z]*/, '').toLowerCase();
+  }
 }
--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
-import { BooksService } from '../shared/services/index';
+import { BookListState, GenreState, CategoryState } from '../shared/states/index';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
   declarations: [HomeComponent],
   exports: [HomeComponent],
-  providers: [BooksService]
+  providers: [BookListState, GenreState, CategoryState]
 })
 export class HomeModule { }
