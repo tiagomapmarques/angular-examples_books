2211cb8c5be6c762ce69f0923286e26d54747877
Tiago Marques <tiago.map.marques@gmail.com>
2016-12-27 20:30:01 +0000

    pages documentation; author search bugfix;

--- src/client/app/book/book.component.ts
+++ src/client/app/book/book.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router }   from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import { BookListState } from '../shared/states/index';
-import { Book } from '../shared/models/index';
+import { BookListState, Book } from '../shared/index';
 
 /**
- * This class represents the lazy loaded HomeComponent.
+ * This class represents the lazy loaded BookComponent.
  */
 @Component({
   moduleId: module.id,
@@ -15,27 +14,42 @@ import { Book } from '../shared/models/index';
 })
 
 export class BookComponent implements OnInit {
+  /**
+   * Book to be displayed in this component
+   */
   private book: Book;
 
   /**
-   * Creates an instance of the HomeComponent with the injected
-   * BooksService.
+   * Creates an instance of the BookComponent.
    *
-   * @param {BooksService} booksService - The injected BooksService.
+   * @param {Router} router - The injected router from Angular2.
+   * @param {ActivatedRoute} route - The injected navigation module from Angular2.
+   * @param {BookListState} bookListState - The injected BookListState.
    */
   constructor(private router: Router, private route: ActivatedRoute, private bookListState: BookListState) {}
 
   /**
-   * Get the names OnInit
+   * On component initialisation, it fetches the id on the URL to find and
+   * display the book.
    */
   public ngOnInit(): void {
     this.route.params.subscribe(params => this.findBook(params['id']));
   }
 
+  /**
+   * Navigates to the root of the application.
+   */
   public goToIndex(): void {
     this.router.navigate(['/']);
   }
 
+  /**
+   * Given a book id, it searches the BookListState for the correct book.
+   * If the book does not exist, it will redirect the user to the root page,
+   * otherwise, it will update the book variable of this component.
+   *
+   * @param {string} id - The id of the book to be searched.
+   */
   private findBook(id: string): void {
     this.bookListState.get().subscribe((books: Book[]) => {
       let filter = books.filter(book => book.id === id);
--- src/client/app/book/book.module.ts
+++ src/client/app/book/book.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
-import { BookComponent } from './book.component';
 import { BookListState } from '../shared/states/index';
+import { BookComponent } from './book.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
